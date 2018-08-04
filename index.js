e77faaf16fd2bc402872cefec52ec4a5be13afe0
Joe Bentley <jbentley@star.sr.bham.ac.uk>
2018-08-04 18:52:27 +0100

    Added mongodb

--- index.js
+++ index.js
@@ -3,18 +3,23 @@ const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
-const messages = [
-  {
-    'color': 'darkgreen',
-    'author': 'Joe',
-    'message': 'Hello Marie!'
-  },
-  {
-    'color': 'darkred',
-    'author': 'Marie',
-    'message': 'Hello Joe, how are you?'
-  }
-];
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true })
+  .then(() => {
+    console.log('Connected to mongodb');
+  })
+  .catch(reason => {
+    console.error(reason);
+    process.exit(-1);
+  });
+
+const messageSchema = new mongoose.Schema({
+  color: String,
+  author: String,
+  message: String
+});
+
+const Message = mongoose.model('Message', messageSchema);
 
 io.on('connection', socket => {
   let username, color;
@@ -25,12 +30,17 @@ io.on('connection', socket => {
   });
 
   socket.on('get-messages', () => {
-    socket.emit('messages', messages);
+    Message.find((err, messages) => {
+      if (err) return console.error(err);
+      socket.emit('messages', messages);
+    });
   });
 
   socket.on('new-message', message => {
-    if (username != null && color != null && username === message.author && color === message.color)
-      messages.push(message);
+    if (username != null && color != null && username === message.author && color === message.color) {
+      const messageDbItem = new Message({color: message.color, author: message.author, message: message.message});
+      messageDbItem.save();
+    }
   });
 });
 
